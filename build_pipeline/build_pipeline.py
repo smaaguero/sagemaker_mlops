901907feb0ddcfc10d1fe074696aca791e1d08d5
Sergio Martinez Aguero <seryii.96@gmail.com>
2025-12-07 15:22:37 +0100

    pipeline working in sagemaker, it trains a model and deploy it depending of the metrics

---
+++ build_pipeline/build_pipeline.py
@@ -0,0 +1,281 @@
+import os
+import boto3
+import sagemaker
+import sagemaker.session
+from sagemaker.workflow.pipeline_context import PipelineSession
+from sagemaker.workflow.parameters import (
+    ParameterInteger,
+    ParameterString,
+)
+from sagemaker.sklearn.processing import SKLearnProcessor
+from sagemaker.processing import ProcessingInput, ProcessingOutput
+from sagemaker.workflow.steps import ProcessingStep
+from sagemaker.estimator import Estimator
+from sagemaker.inputs import TrainingInput
+from sagemaker.workflow.steps import TrainingStep
+from sagemaker.workflow.pipeline import Pipeline
+from sagemaker.model import Model
+from sagemaker.inputs import CreateModelInput
+from sagemaker.workflow.steps import CreateModelStep
+from sagemaker.transformer import Transformer
+from sagemaker.inputs import TransformInput
+from sagemaker.workflow.steps import TransformStep
+from sagemaker.model_metrics import MetricsSource, ModelMetrics 
+from sagemaker.workflow.step_collections import RegisterModel
+from sagemaker.workflow.conditions import ConditionLessThanOrEqualTo
+from sagemaker.workflow.condition_step import ConditionStep
+from sagemaker.workflow.functions import JsonGet
+from sagemaker.processing import ScriptProcessor
+from sagemaker.workflow.properties import PropertyFile
+
+# TODO:
+# Añadir una session al pipeline cuando tenga todo funcionando.
+
+MY_BUCKET = os.getenv("BUCKET")
+MY_ROLE = os.getenv("SAGEMAKER_ROLE_ARN")
+input_data_uri = f"s3://{MY_BUCKET}/abalone/input/abalone-dataset.csv"
+batch_data_uri = f"s3://{MY_BUCKET}/abalone/batch/input/abalone-dataset.csv"
+framework_version = "0.23-1"
+
+# 1. Define the session and role
+region = boto3.Session().region_name
+sagemaker_session = sagemaker.session.Session()
+# role = os.getenv("SAGEMAKER_ROLE_ARN")
+default_bucket = sagemaker_session.default_bucket()
+pipeline_session = PipelineSession()
+model_package_group_name = f"AbaloneModelPackageGroupName"
+
+# 2. Define pipeline parameters
+# processing_instance_count – The instance count of the processing job.
+# input_data – The Amazon S3 location of the input data.
+# batch_data – The Amazon S3 location of the input data for batch transformation.
+# model_approval_status – The approval status to register the trained model with for CI/CD.
+processing_instance_count = ParameterInteger(
+    name="ProcessingInstanceCount",
+    default_value=1
+)
+model_approval_status = ParameterString(
+    name="ModelApprovalStatus",
+    default_value="PendingManualApproval"
+)
+input_data = ParameterString(
+    name="InputData",
+    default_value=input_data_uri,
+)
+batch_data = ParameterString(
+    name="BatchData",
+    default_value=batch_data_uri,
+)
+
+# 3. Define the SKLearn processor to preprocess the data
+sklearn_processor = SKLearnProcessor(
+    framework_version=framework_version,
+    instance_type="ml.m5.xlarge",
+    instance_count=processing_instance_count,
+    base_job_name="sklearn-abalone-process",
+    sagemaker_session=pipeline_session,
+    role=MY_ROLE,
+)
+processor_args = sklearn_processor.run(
+    inputs=[
+      ProcessingInput(source=input_data, destination="/opt/ml/processing/input"),  
+    ],
+    outputs=[
+        ProcessingOutput(output_name="train", source="/opt/ml/processing/train"),
+        ProcessingOutput(output_name="validation", source="/opt/ml/processing/validation"),
+        ProcessingOutput(output_name="test", source="/opt/ml/processing/test")
+    ],
+    code="src/preprocessing.py",
+) 
+
+step_process = ProcessingStep(
+    name="AbaloneProcess",
+    step_args=processor_args
+)
+
+# 4: Define a training step
+model_path = f"s3://{default_bucket}/AbaloneTrain"
+image_uri = sagemaker.image_uris.retrieve(
+    framework="xgboost",
+    region=region,
+    version="1.0-1",
+    py_version="py3",
+    instance_type="ml.m5.xlarge"
+)
+xgb_train = Estimator(
+    image_uri=image_uri,
+    instance_type="ml.m5.xlarge",
+    instance_count=1,
+    output_path=model_path,
+    sagemaker_session=pipeline_session,
+    role=MY_ROLE,
+)
+xgb_train.set_hyperparameters(
+    objective="reg:linear",
+    num_round=50,
+    max_depth=5,
+    eta=0.2,
+    gamma=4,
+    min_child_weight=6,
+    subsample=0.7,
+    silent=0
+)
+train_args = xgb_train.fit(
+    inputs={
+        "train": TrainingInput(
+            s3_data=step_process.properties.ProcessingOutputConfig.Outputs[
+                "train"
+            ].S3Output.S3Uri,
+            content_type="text/csv"
+        ),
+        "validation": TrainingInput(
+            s3_data=step_process.properties.ProcessingOutputConfig.Outputs[
+                "validation"
+            ].S3Output.S3Uri,
+            content_type="text/csv"
+        )
+    },
+)
+
+step_train = TrainingStep(
+    name="AbaloneTrain",
+    step_args = train_args
+)
+
+# 5: Define a processing step for model evaluation
+script_eval = ScriptProcessor(
+    image_uri=image_uri,
+    command=["python3"],
+    instance_type="ml.m5.xlarge",
+    instance_count=1,
+    base_job_name="script-abalone-eval",
+    sagemaker_session=pipeline_session,
+    role=MY_ROLE,
+)
+evaluation_report = PropertyFile(
+    name="EvaluationReport",
+    output_name="evaluation",
+    path="evaluation.json"
+)
+
+eval_args = script_eval.run(
+        inputs=[
+        ProcessingInput(
+            source=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+            destination="/opt/ml/processing/model"
+        ),
+        ProcessingInput(
+            source=step_process.properties.ProcessingOutputConfig.Outputs[
+                "test"
+            ].S3Output.S3Uri,
+            destination="/opt/ml/processing/test"
+        )
+    ],
+    outputs=[
+        ProcessingOutput(output_name="evaluation", source="/opt/ml/processing/evaluation"),
+    ],
+    code="src/evaluation.py",
+)
+
+step_eval = ProcessingStep(
+    name="AbaloneEval",
+    step_args=eval_args,
+    property_files=[evaluation_report],
+)
+
+# 6: Define a CreateModelStep for batch transformation
+model = Model(
+    image_uri=image_uri,
+    model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+    sagemaker_session=pipeline_session,
+    role=MY_ROLE,
+)
+inputs = CreateModelInput(
+    instance_type="ml.m5.large",
+    accelerator_type="ml.eia1.medium",
+)
+step_create_model = CreateModelStep(
+    name="AbaloneCreateModel",
+    model=model,
+    inputs=inputs,
+)
+
+# 7: Define a TransformStep to perform batch transformation
+transformer = Transformer(
+    model_name=step_create_model.properties.ModelName,
+    instance_type="ml.m5.xlarge",
+    instance_count=1,
+    output_path=f"s3://{default_bucket}/AbaloneTransform"
+)
+step_transform = TransformStep(
+    name="AbaloneTransform",
+    transformer=transformer,
+    inputs=TransformInput(data=batch_data)
+)
+
+# 8.Define a RegisterModel step to create a model package
+model_metrics = ModelMetrics(
+    model_statistics=MetricsSource(
+        s3_uri="{}/evaluation.json".format(
+            step_eval.arguments["ProcessingOutputConfig"]["Outputs"][0]["S3Output"]["S3Uri"]
+        ),
+        content_type="application/json"
+    )
+)
+step_register = RegisterModel(
+    name="AbaloneRegisterModel",
+    estimator=xgb_train,
+    model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+    content_types=["text/csv"],
+    response_types=["text/csv"],
+    inference_instances=["ml.t2.medium", "ml.m5.xlarge"],
+    transform_instances=["ml.m5.xlarge"],
+    model_package_group_name=model_package_group_name,
+    approval_status=model_approval_status,
+    model_metrics=model_metrics
+)
+
+# 9: Define a condition step to verify model accuracy
+cond_lte = ConditionLessThanOrEqualTo(
+    left=JsonGet(
+        step_name=step_eval.name,
+        property_file=evaluation_report,
+        json_path="regression_metrics.mse.value"
+    ),
+    right=6.0
+)
+step_cond = ConditionStep(
+    name="AbaloneMSECond",
+    conditions=[cond_lte],
+    if_steps=[step_register, step_create_model, step_transform],
+    else_steps=[], 
+)
+
+# Final step: Define the pipeline
+pipeline_name = f"AbalonePipeline"
+pipeline = Pipeline(
+    name=pipeline_name,
+    parameters=[
+        processing_instance_count,
+        model_approval_status,
+        input_data,
+        batch_data,
+    ],
+    steps=[step_process, step_train, step_eval, step_cond],
+    sagemaker_session=sagemaker_session,
+)
+
+if __name__ == "__main__":
+    # Crear o actualizar pipeline
+    pipeline.upsert(role_arn=MY_ROLE)
+    
+    # Iniciar ejecución
+    execution = pipeline.start(
+        parameters={
+            "InputData": input_data_uri,
+            "BatchData": batch_data_uri,
+            "ProcessingInstanceCount": 1,
+            "ModelApprovalStatus": "PendingManualApproval"
+        },
+        execution_display_name = 'ejecucion-prueba-final'
+    )
